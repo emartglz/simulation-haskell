f6267fe49ea9118286595a7329c78cffadc8a829
kikexd <enrique06660@gmail.com>
2022-01-16 23:23:23 -0500

    board cells now have position

--- src/Board.hs
+++ src/Board.hs
@@ -1,9 +1,7 @@
 module Board (board, addCorralsBoard, addObstaclesBoard, filterByTypeCell) where
 
 import Random (rand, randomRange, runRandom)
-import Utils (replace, _1)
-
-type Position = (Int, Int)
+import Utils (getBoardCell, getCellType, getColumn, getPosition, getRow, replace)
 
 type CellType = String
 
@@ -11,34 +9,41 @@ type Pick = Bool
 
 type Drop = Bool
 
+type Position = (Int, Int)
+
 type BoardCell = (CellType, Pick, Drop)
 
 type PositionBoardCell = (Position, BoardCell)
 
-type Board = [[BoardCell]]
+type Board = [[PositionBoardCell]]
 
 board :: Int -> Int -> Board
-board r c
-  | r == 0 || c == 0 = []
-  | otherwise = boardRow r c : board (r - 1) c
+board rEnd cEnd
+  | rEnd == 0 || cEnd == 0 = []
+  | otherwise = boardFormer 0 rEnd cEnd
 
-boardRow :: Int -> Int -> [BoardCell]
-boardRow r c
-  | c == 0 = []
-  | otherwise = ("empty", False, False) : boardRow r (c - 1)
+boardFormer :: Int -> Int -> Int -> Board
+boardFormer r rEnd cEnd
+  | r == rEnd = []
+  | otherwise = boardRow r 0 cEnd : boardFormer (r + 1) rEnd cEnd
 
-boardCellTypeEncounter :: CellType -> Board -> [(Int, Int)]
+boardRow :: Int -> Int -> Int -> [PositionBoardCell]
+boardRow r c cEnd
+  | c == cEnd = []
+  | otherwise = ((r, c), ("empty", False, False)) : boardRow r (c + 1) cEnd
+
+boardCellTypeEncounter :: CellType -> Board -> [PositionBoardCell]
 boardCellTypeEncounter cellType board = boardCellTypeEncounterRow cellType board 0 (length board)
 
-boardCellTypeEncounterRow :: CellType -> Board -> Int -> Int -> [(Int, Int)]
+boardCellTypeEncounterRow :: CellType -> Board -> Int -> Int -> [PositionBoardCell]
 boardCellTypeEncounterRow cellType board r rEnd
   | r == rEnd = []
   | otherwise = boardCellTypeEncounterColmn cellType board r 0 (length (board !! r)) ++ boardCellTypeEncounterRow cellType board (r + 1) rEnd
 
-boardCellTypeEncounterColmn :: CellType -> Board -> Int -> Int -> Int -> [(Int, Int)]
+boardCellTypeEncounterColmn :: CellType -> Board -> Int -> Int -> Int -> [PositionBoardCell]
 boardCellTypeEncounterColmn cellType board r c cEnd
   | c == cEnd = []
-  | _1 (board !! r !! c) == cellType = (r, c) : boardCellTypeEncounterColmn cellType board r (c + 1) cEnd
+  | getCellType (getBoardCell (board !! r !! c)) == cellType = (board !! r !! c) : boardCellTypeEncounterColmn cellType board r (c + 1) cEnd
   | otherwise = boardCellTypeEncounterColmn cellType board r (c + 1) cEnd
 
 addObstaclesBoard :: Int -> Int -> Board -> Board
@@ -48,32 +53,32 @@ addObstaclesBoard seed amount board
   where
     emptyCells = boardCellTypeEncounter "empty" board
     randomCell = randomRange 0 (length emptyCells - 1) (runRandom rand seed)
-    (rR, rC) = emptyCells !! randomCell
-    boardR = replace board rR (replace (board !! rR) rC ("obstacle", False, False))
+    ((rR, rC), _) = emptyCells !! randomCell
+    boardR = replace board rR (replace (board !! rR) rC ((rR, rC), ("obstacle", False, False)))
 
-get4AdyacentCells :: (Int, Int) -> Board -> [PositionBoardCell]
+get4AdyacentCells :: Position -> Board -> [PositionBoardCell]
 get4AdyacentCells (r, c) board = result
   where
     rows = length board
     columns = length (head board)
     up
       | r == 0 = []
-      | otherwise = [((r -1, c), board !! (r - 1) !! c)]
+      | otherwise = [board !! (r - 1) !! c]
     down
       | r == (rows - 1) = []
-      | otherwise = [((r + 1, c), board !! (r + 1) !! c)]
+      | otherwise = [board !! (r + 1) !! c]
     left
       | c == 0 = []
-      | otherwise = [((r, c - 1), board !! r !! (c - 1))]
+      | otherwise = [board !! r !! (c - 1)]
     right
       | c == (columns - 1) = []
-      | otherwise = [((r, c + 1), board !! r !! (c + 1))]
+      | otherwise = [board !! r !! (c + 1)]
     result = up ++ down ++ left ++ right
 
 filterByTypeCell :: CellType -> [PositionBoardCell] -> [PositionBoardCell]
 filterByTypeCell _ [] = []
 filterByTypeCell cellType ((p, x) : xs)
-  | _1 x == cellType = (p, x) : filterByTypeCell cellType xs
+  | getCellType x == cellType = (p, x) : filterByTypeCell cellType xs
   | otherwise = filterByTypeCell cellType xs
 
 addCorralsBoard :: Int -> Int -> Board -> Board
@@ -82,22 +87,22 @@ addCorralsBoard seed amount board
   | null corralsAlready =
     let emptyCells = boardCellTypeEncounter "empty" board
         randomCell = randomRange 0 (length emptyCells - 1) (runRandom rand seed)
-        (rR, rC) = emptyCells !! randomCell
-        boardR = replace board rR (replace (board !! rR) rC ("corral", False, False))
+        ((rR, rC), _) = emptyCells !! randomCell
+        boardR = replace board rR (replace (board !! rR) rC ((rR, rC), ("corral", False, False)))
      in addCorralsBoard rR (amount - 1) boardR
   | otherwise =
     let corralCells = boardCellTypeEncounter "corral" board
-        corralWithAdyacents = map (\(r, c) -> ((r, c), get4AdyacentCells (r, c) board)) corralCells
-        corralWithEmptyAdyacents = map (\(p, l) -> (p, filterByTypeCell "empty" l)) corralWithAdyacents
-        corralWithEmptyAdyacentsNotNull = filter (\(p, l) -> not (null l)) corralWithEmptyAdyacents
+        corralWithAdyacents = map (\((r, c), x) -> (((r, c), x), get4AdyacentCells (r, c) board)) corralCells
+        corralWithEmptyAdyacents = map (\(x, l) -> (x, filterByTypeCell "empty" l)) corralWithAdyacents
+        corralWithEmptyAdyacentsNotNull = filter (\(_, l) -> not (null l)) corralWithEmptyAdyacents
 
         randomCorral = randomRange 0 (length corralWithEmptyAdyacentsNotNull - 1) (runRandom rand seed)
-        ((rR, rC), l) = corralWithEmptyAdyacentsNotNull !! randomCorral
+        (((rR, rC), _), l) = corralWithEmptyAdyacentsNotNull !! randomCorral
 
         randomEmpty = randomRange 0 (length l - 1) (runRandom rand rR)
         ((rER, rEC), _) = l !! randomEmpty
 
-        boardR = replace board rER (replace (board !! rER) rEC ("corral", False, False))
+        boardR = replace board rER (replace (board !! rER) rEC ((rER, rEC), ("corral", False, False)))
      in addCorralsBoard rER (amount - 1) boardR
   where
     corralsAlready = boardCellTypeEncounter "corral" board
--- src/Utils.hs
+++ src/Utils.hs
@@ -1,7 +1,19 @@
-module Utils (replace, _1) where
+module Utils (replace, getPosition, getBoardCell, getRow, getColumn, getCellType) where
 
 replace :: [a] -> Int -> a -> [a]
 replace list index element = let (first, x : xs) = splitAt index list in first ++ (element : xs)
 
-_1 :: (a, b, c) -> a
-_1 (a, b, c) = a
+getPosition :: (p, c) -> p
+getPosition (p, c) = p
+
+getBoardCell :: (p, c) -> c
+getBoardCell (p, c) = c
+
+getCellType :: (String, Bool, Bool) -> String
+getCellType (c, p, d) = c
+
+getRow :: (Int, Int) -> Int
+getRow (r, c) = r
+
+getColumn :: (Int, Int) -> Int
+getColumn (r, c) = c
